37c08d719a25fd2f170c0cb5665d0772a7a9dc22
chipo phiri <chipoephiri@gmail.com>
2021-08-26 16:32:19 +0200

    initial commit

---
+++ table-generator.py
@@ -0,0 +1,83 @@
+class TableGenerator(object):
+    def readFile(self, file):
+        self.results = {}
+        self.file = file
+
+        with open(self.file, 'r') as file:
+            for line in file:
+
+                self.splitLine = line.split()
+
+                # print(self.splitLine)
+
+                self.team1 = self.splitLine[0]
+                self.team2 = self.splitLine[-2]
+                # line = file.readline()
+                if (int(self.splitLine[1][0]) > int(self.splitLine[-1])):
+
+                    if self.team1 in self.results:
+                        self.results[self.team1] += 3
+                    else:
+                        self.results.update({self.team1: 3})
+
+                    if self.team2 in self.results:
+                        self.results[self.team2] += 0
+                    else:
+                        self.results.update({self.team2: 0})
+
+                    if int(self.splitLine[1][0]) == int(self.splitLine[-1]):
+
+                        if self.team1 in self.results:
+                            self.results[self.team1] += 1
+                        else:
+                            self.results.update({self.team1: 1})
+
+                        if self.team2 in self.results:
+                            self.results[self.team2] += 1
+                        else:
+                            self.results.update({self.team2: 1})
+
+                    if int(self.splitLine[1][0]) < int(self.splitLine[-1]):
+
+                        if self.team1 in self.results:
+                            self.results[self.team1] += 0
+                        else:
+                            self.results.update({self.team1: 0})
+
+                        if self.team2 in self.results:
+                            self.results[self.team2] += 3
+                        else:
+                            self.results.update({self.team2: 3})
+
+            allResults = self.results
+            print(allResults)
+            return allResults
+
+
+class ShowRanking:
+    soccer_results = TableGenerator()
+    sorted_scores = []
+
+    def sortResults(self):
+        unsorted = self.soccer_results.readFile('input.txt')
+        self.sorted_scores = sorted(
+            unsorted.items(), key=lambda x: x[1], reverse=True)
+        print(self.sorted_scores)
+        return self.sorted_scores
+
+    def displayResults(self):
+        sorted_results = self.sortResults()
+        print(sorted_results)
+        with open('output.txt', 'w') as f:
+            for idx, team in enumerate(sorted_results):
+                f.write('{0}. {1}, {2} pts\n'.format(
+                    idx + 1, team[0], team[1]))
+
+
+def main():
+    finalTable = ShowRanking()
+    finalTable.displayResults()
+
+
+if __name__ == "__main__":
+    main()
